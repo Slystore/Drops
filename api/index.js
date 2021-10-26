a3b3329d4d1e1ce052f169c4470cf6e0ef23139e
Slystore <gabriel776977@gmail.com>
2021-10-26 12:44:14 -0300

    husky added to dependencies

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
-const server = require('./src/app')
-const {conn} = require('./src/db')
+const server = require("./src/app");
+const { conn } = require("./src/db");
 
-conn.sync({force:true})
-    .then(async() => {
-        console.log('DB connected!')
-        server.listen(3001,() => console.log('Server listen port 3001'))
-    }).catch((e)=> console.log('connection falied',e))
+conn
+  .sync({ force: true })
+  .then(async () => {
+    console.log("DB connected!");
+    server.listen(3001, () => console.log("Server listen port 3001"));
+  })
+  .catch((e) => console.log("connection falied", e));
