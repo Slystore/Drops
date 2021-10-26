6148091badce0ada8a570cf993da932a8a1105c4
BtoMJ <danny_mj17@hotmail.com>
2021-10-26 14:33:55 -0500

    Agregando dependencias y Homeview

---
+++ client/src/components/Brand/Brand.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Brand() {
+    return (
+        <div className="ContainerBrand">
+            Brand
+        </div>
+    )
+}
+
+export default Brand
--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
+import NavBar from '../components/NavBar/NavBar';
+import Brand from '../components/Brand/Brand';
 import Slider from '../components/Slider/Slider';
 import Home from '../components/Home/Home';
 import Footer from '../components/Footer/Footer';
-import NavBar from '../components/NavBar/NavBar';
 
 function HomeView() {
     return (
         <div className='Container'>
             <NavBar />
+            <Brand />
             <Slider />
             <Home />
             <Footer />
