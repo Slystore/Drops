00b344d782e09274a8162cc549b16d5a08b39515
ThestalkerN-byte <82970148+ThestalkerN-byte@users.noreply.github.com>
2021-10-26 21:10:11 -0300

    Register complete
    Termine el register

---
+++ api/src/controllers/User-Controller/user-Log.js
@@ -0,0 +1,5 @@
+const login = async (req,res)=> {
+    let {mail,password} = req.body;
+}
+
+module.exports = login
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-
+const users = require('../routes/User/User')
 const app = express.Router()
 
+app.use('/',users)
 
 module.exports = app; 
