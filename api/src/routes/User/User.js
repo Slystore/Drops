1363c48fb50d4b46e5f187ced8db20ad8cbe56ab
ThestalkerN-byte <82970148+ThestalkerN-byte@users.noreply.github.com>
2021-10-27 13:23:51 -0300

    User put funcionando

--- api/src/routes/User/User.js
+++ api/src/routes/User/User.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const login = require('../../controllers/User-Controller/user-Log')
 const register = require('../../controllers/User-Controller/user-Register')
+const putUser = require('../../controllers/User-Controller/user-Put')
 
 const router = express.Router()
 
+router.put('/edit/:id',putUser)
 router.post('/register',register)
 router.post('/login',login)
 
