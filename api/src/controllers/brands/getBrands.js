ef1972f6bb981497089beedda97c86dc2ceb5759
ThestalkerN-byte <82970148+ThestalkerN-byte@users.noreply.github.com>
2021-10-29 12:54:43 -0300

    Crud Brand complete
    Complete el crud de las marcas
    Agruegue la busqueda por query y por params en el get que estaba  y cree el put y post

--- api/src/controllers/brands/getBrands.js
+++ api/src/controllers/brands/getBrands.js
@@ -1,12 +1,49 @@
-const { Brand } = require('../../db.js');
+const { Op } = require("sequelize");
+const { Brand } = require("../../db.js");
 
-const getBrands = async(req, res, next) => {
-    try {
-        const allBrands = await Brand.findAll({});
-        return res.status(200).json(allBrands);
-    } catch (err) {
-        next(err)
+const getBrands = async (req, res, next) => {
+  try {
+    //get por id
+    const { id } = req.params;
+    if (id) {
+      let brandOne = await Brand.findOne({
+        where: {
+          id: id,
+        },
+        atributtes: ["name", "id"],
+      });
+      if (brandOne) {
+        return res.status(200).json(brandOne);
+      } else {
+        return res
+          .status(404)
+          .json({ msg: "No se a encontrado una marca con ese id" });
+      }
     }
+    //get por query
+    const { name } = req.query;
+    if (name) {
+      name.toLowerCase().trim();
+      let brandName = await Brand.findOne({
+        where: {
+          name: {
+            [Op.iLike]: `%${name}%`,
+          },
+        },
+      });
+      if (brandName) return res.status(200).json(brandName);
+      else
+        return res
+          .status(404)
+          .json({ msg: "No se a encontrado el nombre de la marca buscada" });
+    }
+
+    //get all
+    const allBrands = await Brand.findAll({});
+    return res.status(200).json(allBrands);
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports = getBrands
+module.exports = getBrands;
