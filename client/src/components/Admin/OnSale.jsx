44e52cd1b2e7b4f8e06492015930eaee502ce50f
juanitu82 <76759029+juanitu82@users.noreply.github.com>
2021-11-01 10:56:24 -0300

    rooteo admin

---
+++ client/src/components/Admin/OnSale.jsx
@@ -0,0 +1,9 @@
+const OnSale = () => {
+    return(
+        <div>
+            <div style={{height: '200px', width: '400px', margin: '25% auto', border: '1px black solid'}}> <h2> Mapeo de ofertas </h2></div>
+        </div>
+    )
+}
+
+export default OnSale
