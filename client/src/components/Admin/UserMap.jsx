49c89d8c1a401e4325a8a82cdf0707d8202aee17
juanitu82 <76759029+juanitu82@users.noreply.github.com>
2021-11-01 21:37:38 -0300

    users admin panel

---
+++ client/src/components/Admin/UserMap.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const ProductMap = (props) => {
+    return(
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+            <img src={props.profileImg} alt={props.id} style={{height:'25px', width:'50px'}}/>
+            <p>{`${props.name} ${props.surname}`}</p>
+            <p>{props.mail}</p>
+            <p>{props.password}</p>
+            <p>{props.phone}</p>
+            <p>{props.status}</p>
+            <p>{props.userType}</p>
+            <Link to={`admin/product/${props.id}/update`}> <a> Reset Password </a></Link> 
+           
+        </div>
+    )
+}
+
+export default ProductMap
--- client/src/components/Admin/Users.jsx
+++ client/src/components/Admin/Users.jsx
@@ -1,7 +1,33 @@
+
+import { useEffect, useState } from "react" 
+import { useSelector, useDispatch } from "react-redux"
+import { getUsers } from "../../redux/users/userActions"
+import UserMap from "./UserMap"
+
 const Users = () => {
+    const dispatch = useDispatch()
+    
+    useEffect(()=>{
+        dispatch(getUsers())
+    },[dispatch])
+    
+    const usuarios = useSelector( state => state.usersReducer.users.users);
+
     return(
         <div>
-            <div style={{height: '200px', width: '400px', margin: '25% auto', border: '1px black solid'}}> <h2> Mapeo de usuarios </h2></div>
+            <div > 
+                <h2> Mapeo de usuarios </h2>
+                <div>
+                    {
+                        usuarios && usuarios.map(user => (
+                        <div>
+                            
+                            <UserMap key={user.id} name={user.name} surname={user.surname} image={user.profileImg} mail={user.mail} password={user.password} type={user.userType} />
+                        </div>
+                    ))}
+                
+                </div>
+            </div>
         </div>
     )
 }
