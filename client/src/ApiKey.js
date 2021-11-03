25e28370c428726f8b57232c102d172e728f0f8f
BtoMJ <danny_mj17@hotmail.com>
2021-11-03 09:39:47 -0600

    Formulario de contacto Landing

---
+++ client/src/ApiKey.js
@@ -0,0 +1,6 @@
+//archivo ApiKey.js
+export default { 
+    USER_ID: 'user_iQ30OC1wzQq60J29vHHve', 
+    TEMPLATE_ID: 'template_fbs7gfi',
+    SERVICE_ID: 'service_ozfmn4p',
+    }
--- client/src/components/Admin/OnSale.jsx
+++ client/src/components/Admin/OnSale.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const OnSale = () => {
     return(
         <div>
