82ad39374f8be959b437b2c0efeda658525ddea8
Slystore <gabriel776977@gmail.com>
2021-11-04 12:50:37 -0300

    PostOrder and putOrder controller & getOrder finished

--- api/src/controllers/order/postOrder.js
+++ api/src/controllers/order/postOrder.js
@@ -9,63 +9,58 @@ const postOrder = async (req, res) => {
     merchantOrderId,
     status,
     userId,
-    cart,
+    cart, //[{ productId, price, quantity }]
   } = req.body;
-  
 
   try {
-      const getOrder = await Orders.findOne({
+    const getOrder = await Orders.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!getOrder) {
+      const user = await Users.findOne({
         where: {
           id: userId,
         },
       });
 
-      if (!getOrder) {
-        const user = await Users.findOne({
-          where: {
-            id: userId,
-          },
-        });
-  
-        if (!user) {
-          return res.status(404).json({
-            message: "User not found",
-          });
-        }
-
-        const order = await Orders.create({
-          date: new Date().toISOString(),
-          location,
-          totalPrice,
-          paymentId,
-          paymentStatus,
-          merchantOrderId,
-          status: status || "pending",
-          userId,
-          cart
-        });
-        await user.addOrders(order.id);
-        await order.setUser(user.id);
-
-        cart.forEach(async (el) => {
-          try{
-            const productData = await Product.findByPk(el.id);
-            await OrderDetail.create({
-              quantity: el.quantity,
-              price: el.price,
-              orderId: order.id,
-              productId: productData.id,
-            });
-          }catch(err){
-            console.log(err)
-          }
-        });
-        res.status(200).json({
-          message: "Order created successfully",
-          order: order
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
         });
       }
 
+      const order = await Orders.create({
+        date: new Date().toISOString(),
+        location,
+        totalPrice,
+        paymentId,
+        paymentStatus,
+        merchantOrderId,
+        status: status || "pending",
+        userId,
+        cart,
+      });
+      await user.addOrders(order.id);
+
+      cart?.forEach(async (el) => {
+        const productData = await Product.findByPk(el.productId);
+        order.addProduct(productData.id, {
+          through: {
+            price: productData.price,
+            quantity: el.quantity,
+          }
+          });
+      });
+      
+      return res.json({
+        status: `${order.status}`,
+        message: true,
+        cartId: `${order.id}`
+      });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({
--- api/src/routes/order/order.js
+++ api/src/routes/order/order.js
@@ -3,8 +3,11 @@ const router = Router();
 
 const getOrder = require('../../controllers/order/getOrder.js');
 const postOrder = require('../../controllers/order/postOrder.js');
+const updateOrder = require('../../controllers/order/putOrder.js');
+
 
 router.get('/', getOrder);
 router.post('/createOrder', postOrder);
+router.put('/updateOrder/:id', updateOrder);
 
 module.exports = router
