e128a701fd21f0b5be58d2d2ba58706ae97c58b8
BtoMJ <danny_mj17@hotmail.com>
2021-11-13 12:59:59 -0600

    Views de productos por categoría

--- client/src/views/categories/sandalias.jsx
+++ client/src/views/categories/sandalias.jsx
@@ -2,10 +2,16 @@ import { useSelector, useDispatch } from "react-redux";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getProducts } from "../../redux/products/productsAction";
-import Product from "../../components/Product/Product"
-import Paginado from '../../components/Catalogue/Paginado'
+import Product from "../../components/Product/Product";
+import Paginado from '../../components/Catalogue/Paginado';
+import NavBar from '../../components/NavBar/NavBar';
+import Footer from '../../components/Footer/Footer';
+import VanillaTilt from 'vanilla-tilt';
 
 const Sandalias = () => {
+
+    const element = document.querySelectorAll(".Shoes");
+    VanillaTilt.init(element);
 
     const dispatch = useDispatch();
 
@@ -14,52 +20,64 @@ const Sandalias = () => {
         
     }, [dispatch]);
 
-  const { products } = useSelector((state) => state.productReducer);
-  
-  const data = products.filter(e => Object.values(e.Category).includes('Sandalias'))
+    const { products } = useSelector((state) => state.productReducer);
+    
+    const data = products.filter(e => Object.values(e.Category).includes('Sandalias'))
 
-  const [currPage, setCurrPage] = useState(1);
-  const [cardsxPage, ] = useState(10);
-  
-  const lastProduct = currPage * cardsxPage
-  const firstProduct =  lastProduct - cardsxPage;
+    const [currPage, setCurrPage] = useState(1);
+    const [cardsxPage, ] = useState(8);
+    
+    const lastProduct = currPage * cardsxPage
+    const firstProduct =  lastProduct - cardsxPage;
 
-  const currProducts = data.slice(firstProduct, lastProduct);
-  
-  const paginado = (pagNumber) => {
-      setCurrPage(pagNumber)
-  }
+    const currProducts = data.slice(firstProduct, lastProduct);
+    
+    const paginado = (pagNumber) => {
+        setCurrPage(pagNumber)
+    }
 
     return(
-        <div>
-            <div > 
-            <h2> Mapeo de Sandalias </h2>
-            <Link to='/'><a >volver</a></Link>
-
-            <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', justifyContent: 'center', alignItems: 'center'}}>
-            { currProducts && currProducts.map(e => 
-                <Product 
-                id={e.id}
-                image={e.image}
-                name={e.name}
-                price={e.price}
-                status={e.status}
-                description={e.description}
-                Sizes = {e.Sizes}  
-                onSale={e.onSale}
-                discounts={e.Discounts}
-                />)}
-            
+        <div className="BrandContainer">
+            <NavBar />
+            <div className="CategoryImageFlipFlop">
+                <h3>Sandalias</h3>
             </div>
-
-             <div className="Paginado">
-                <Paginado 
-                cardsxPage={cardsxPage} 
-                products={data.length}
-                paginado={paginado} 
-                />
-          </div>
+            <div className="BrandProducts">
+                {
+                    currProducts && currProducts.map((e,index) => 
+                        <Link to={`/catalogue/${e.id}`} key={index}>
+                            <div className="Shoes" data-tilt >
+                                {
+                                    <Product 
+                                    id={e.id}
+                                    image={e.image}
+                                    name={e.name}
+                                    price={e.price}
+                                    status={e.status}
+                                    description={e.description}
+                                    Sizes = {e.Sizes}  
+                                    onSale={e.onSale}
+                                    discounts={e.Discounts}
+                                    />
+                                }
+                            </div>
+                        </Link>
+                    )
+                }
             </div>
+            {
+                currPage > 1? 
+                    <div className="Paginado">
+                            <Paginado 
+                            cardsxPage={cardsxPage} 
+                            products={data.length}
+                            paginado={paginado} 
+                            />
+                    </div>
+                : 
+                null
+            }
+            <Footer />
         </div>
     )
 }
