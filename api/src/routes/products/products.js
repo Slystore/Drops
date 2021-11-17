9aed58163d6e90e25ca567388b42c118e9d209a9
Slystore <gabriel776977@gmail.com>
2021-11-17 17:58:57 -0300

    graphics quantity product added to admin panel

--- api/src/routes/products/products.js
+++ api/src/routes/products/products.js
@@ -1,17 +1,23 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const getProducts = require('../../controllers/product/getProducts.js');
-const getProductById = require('../../controllers/product/getProductById.js');
-const {postProduct} = require('../../controllers/product/postProduct.js');
-const updateProduct = require('../../controllers/product/putProduct.js');
-const deleteProduct = require('../../controllers/product/deleteProduct.js');
+const multer = require("multer");
+const  storageMulter  = require("./storageMulter.js");
+const upload = multer({ storage: storageMulter });
 
+const getProducts = require("../../controllers/product/getProducts.js");
+const getProductById = require("../../controllers/product/getProductById.js");
+const { postProduct } = require("../../controllers/product/postProduct.js");
+const updateProduct = require("../../controllers/product/putProduct.js");
+const deleteProduct = require("../../controllers/product/deleteProduct.js");
 
-router.get('/', getProducts);
-router.get('/:id', getProductById);
-router.post('/createProduct', postProduct);
-router.put('/updateProduct', updateProduct);
-router.put('/deleteProduct/:id', deleteProduct);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.post("/createProduct", postProduct);
+router.post('/addProductPhoto', upload.single('image'), (req, res) => {
+    res.json(req.file);
+});
+router.put("/updateProduct", updateProduct);
+router.put("/deleteProduct/:id", deleteProduct);
 
-module.exports = router
+module.exports = router;
