fc9227e65df5e12d94d953ec28b93e74dd5b2c53
Slystore <gabriel776977@gmail.com>
2021-11-17 18:05:53 -0300

    product routes fixed

--- api/src/routes/products/products.js
+++ api/src/routes/products/products.js
@@ -10,12 +10,6 @@ const putDiscountsById = require('../../controllers/discounts/putDiscountsById')
 const putForDiscount = require('../../controllers/discounts/putForDiscounts');
 const discountUnsubscribe = require('../../controllers/discounts/discountUnsubscribe');
 
-const getProducts = require("../../controllers/product/getProducts.js");
-const getProductById = require("../../controllers/product/getProductById.js");
-const { postProduct } = require("../../controllers/product/postProduct.js");
-const updateProduct = require("../../controllers/product/putProduct.js");
-const deleteProduct = require("../../controllers/product/deleteProduct.js");
-
 router.get('/', getProducts);
 router.get('/:id', getProductById);
 router.post('/createProduct', postProduct);
